a7aceba75d6cc13d4d6534f7ceeedd3734f158ec
Vijay-Mundaragi <saivijay010@gmail.com>
2018-12-27 18:01:09 +0530

    corrected typo

--- config.py
+++ config.py
@@ -6,5 +6,6 @@ load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
     SECRET_KEY = os.getenv('SECRET_KEY') or 'I am the best...!'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or os.environ.get('SQLALCHEMY_DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or os.environ.get('SQLALCHEMY_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = os.environ.get('SQLALCHEMY_TRACK_MODIFICATIONS') or False
+
