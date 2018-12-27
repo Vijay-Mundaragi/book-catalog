5f1ffbd2008d0c578e45d743161b360ea4cfdfa1
Vijay-Mundaragi <saivijay010@gmail.com>
2018-12-27 16:56:39 +0530

    corrections

--- run.py
+++ run.py
@@ -1,10 +1,7 @@
 from app import create_app, db
 
+my_app = create_app()
+with my_app.app_context():
+    db.create_all()
 
-if __name__ == "__main__":
-    my_app = create_app('prod')
-    with my_app.app_context():
-        db.create_all()
-
-    my_app.run()
-    
+my_app.run()
